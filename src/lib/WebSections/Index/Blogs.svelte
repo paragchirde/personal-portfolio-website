a803b202d26c506d705be863dc718e89c80c9284
Parag Chirde <parag.chirde@gmail.com>
2023-03-30 14:35:08 +0530

    update for - blog section

---
+++ src/lib/WebSections/Index/Blogs.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let blogs;
+	import BlogCard from '$lib/Cards/Blog.svelte';
+</script>
+
+<div class="grid grid-flow-row grid-cols-12 border-t border-collapse font-light">
+	<div class="col-span-0 lg:col-span-2 border-r" />
+	<div class=" col-span-12 lg:col-span-8 w-full p-4 flex justify-between items-center ">
+		<div class="grid grid-flow-row grid-cols-2 lg:grid-cols-4 gap-2 lg:gap-4">
+			<!-- svelte-ignore empty-block -->
+			{#each blogs as blog}
+                <a href={blog.link} target="_blank" rel="noopener noreferrer">
+                    <div class="border flex flex-col h-full">
+                        <div>
+                            <img class="h-16 lg:h-24 w-full object-cover" alt={blog.title} src={blog.thumbnail}/>
+                        </div>
+                        <div class="p-2 lg:p-4">
+                            <p class="text-sm lg:text-sm font-base"> {blog.title} </p>
+                        </div>
+                        <div class="px-2 lg:px-4 pb-4">
+                            <div class="text-xxs lg:text-xxs font-base flex flex-wrap gap-2">
+                                {#each blog.categories as c }
+                                       <div class="rounded-full px-4 py-0.5 border"> {c} </div> 
+                                {/each}
+                            </div>
+                        </div>
+                    </div>
+                </a>
+			{/each}
+		</div>
+	</div>
+	<div class="border-r border-l col-span-0 lg:col-span-2 p-2 " />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,26 @@
 <script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch(
+			'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fparagchirde.medium.com%2Ffeed'
+		);
+		const data = await res.json();
+		console.log(data);
+		if (res.ok) {
+			return {
+				props: { blogs: data.items }
+			};
+		}
+	}
 	export const prerender = true;
-	import Row1 from '../lib/WebSections/Index/Row1.svelte'
-	import Row2 from '../lib/WebSections/Index/Row2.svelte'
-	import Row3 from '../lib/WebSections/Index/Row3.svelte'
-	import Skills from '../lib/WebSections/Index/Skills.svelte'
+	import Row1 from '../lib/WebSections/Index/Row1.svelte';
+	import Row2 from '../lib/WebSections/Index/Row2.svelte';
+	import Row3 from '../lib/WebSections/Index/Row3.svelte';
+	import Skills from '../lib/WebSections/Index/Skills.svelte';
+	import Blogs from '../lib/WebSections/Index/Blogs.svelte';
 </script>
 
 <script>
+	export let blogs;
 </script>
 
 <svelte:head>
@@ -32,11 +46,11 @@
 <section class="overflow-hidden 2xl:container">
 	<div class="border">
 		<!-- Row 1  - The Navbar -->
-		<Row1></Row1>
+		<Row1 />
 		<!-- Row 2 - The Profile -->
-		<Row2></Row2>
+		<Row2 />
 		<!-- Row 3 - The forte -->
-		<Row3></Row3>
+		<Row3 />
 		<!-- Row 4 -->
 		<div class="grid grid-flow-row grid-cols-12 border-t border-collapse font-light h-16">
 			<div class="col-span-2 border-r" />
@@ -55,33 +69,55 @@
 		<!-- Row Skills -->
 		<div class="grid grid-flow-row grid-cols-12 border-t border-collapse font-light h-16">
 			<div class="lg:col-span-2 border-r" />
-			<div
-				class=" col-span-7 lg:col-span-3 w-full p-4"
-			>
-				<span class="text-md lg:text-lg font-semibold uppercase">Technical Skills<span class="text-rose-400">.</span> </span>
+			<div class=" col-span-7 lg:col-span-3 w-full p-4">
+				<span class="text-md lg:text-lg font-semibold uppercase"
+					>Technical Skills<span class="text-rose-400">.</span>
+				</span>
 			</div>
 			<div class="border-r pt-0 pr-0 col-span-2 h-full " />
 			<div class=" hidden lg:block border-r col-span-2 p-2 " />
 			<div class="border-r col-span-2 p-2 " />
 		</div>
 		<!-- Row 5 - Skills Quote -->
-		<Skills></Skills>
-		<!-- Row 6 -->
-		<div class="grid grid-flow-row grid-cols-12 border-t border-collapse font-light h-32">
-			<div class="col-span-2 border-r" />
-			<div class="" />
-			<div class="border-r  col-span-2 relative">
-				<div class="grid grid-flow-row grid-cols-4 h-full">
-					<div class="border-r h-full col-span-2" />
-					<div class="border-r h-full " />
-					<div class="	h-full " />
+		<Skills />
+		<!-- Row Blogs -->
+		<div class="grid grid-flow-row grid-cols-12 border-t border-collapse font-light h-16">
+			<div class="lg:col-span-2 border-r" />
+			<div class=" col-span-7 lg:col-span-3 w-full p-4 flex justify-between items-center">
+				<p>
+					<span class="text-md lg:text-lg font-semibold uppercase"
+						>Blogs<span class="text-rose-400">.</span>
+					</span>
+				</p>
+				<div class="flex flex-row space-x-2 justify-end items-center w-full">
+					<p>
+						<a href="https://paragchirde.medium.com/" target="_blank" rel="noopener noreferrer">
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 0 24 24"
+								><path
+									fill="currentColor"
+									d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zm-2.046 4.265l-.966.925a.28.28 0 0 0-.106.271v6.801a.28.28 0 0 0 .106.271l.942.926v.204h-4.741v-.204l.977-.948c.097-.096.097-.125.097-.271V9.742l-2.716 6.896h-.368L8.018 9.742v4.622a.644.644 0 0 0 .176.531l1.271 1.541v.203H5.861v-.203l1.271-1.541a.618.618 0 0 0 .164-.531V9.02a.468.468 0 0 0-.152-.395l-1.13-1.36v-.203H9.52l2.71 5.943l2.382-5.943h3.343v.203z"
+								/></svg
+							>
+						</a>
+					</p>
+					<p>
+						<a href="https://dev.to/chirdeparag" target="_blank" rel="noopener noreferrer">
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 0 24 24"
+								><path
+									fill="currentColor"
+									d="M7.826 10.083a.784.784 0 0 0-.468-.175h-.701v4.198h.701a.786.786 0 0 0 .469-.175c.155-.117.233-.292.233-.525v-2.798c.001-.233-.079-.408-.234-.525zM19.236 3H4.764C3.791 3 3.002 3.787 3 4.76v14.48c.002.973.791 1.76 1.764 1.76h14.473c.973 0 1.762-.787 1.764-1.76V4.76A1.765 1.765 0 0 0 19.236 3zM9.195 13.414c0 .755-.466 1.901-1.942 1.898H5.389V8.665h1.903c1.424 0 1.902 1.144 1.903 1.899v2.85zm4.045-3.562H11.1v1.544h1.309v1.188H11.1v1.543h2.142v1.188h-2.498a.813.813 0 0 1-.833-.792V9.497a.813.813 0 0 1 .792-.832h2.539l-.002 1.187zm4.165 4.632c-.531 1.235-1.481.99-1.906 0l-1.548-5.818h1.309l1.193 4.569l1.188-4.569h1.31l-1.546 5.818z"
+								/></svg
+							>
+						</a>
+					</p>
 				</div>
 			</div>
-			<div class="border-r pt-0 pr-0 col-span-3 h-full flex flex-col relative" />
-			<div class="border-r col-span-2 p-2 " />
+			<div class="border-r pt-0 pr-0 col-span-2 h-full " />
+			<div class=" hidden lg:block border-r col-span-2 p-2 " />
 			<div class="border-r col-span-2 p-2 " />
 		</div>
-		<!--  -->
+		<!-- Row Blogs Data -->
+		<Blogs {blogs} />
 	</div>
 </section>
 
